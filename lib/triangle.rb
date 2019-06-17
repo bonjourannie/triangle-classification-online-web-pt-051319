a1d550695b083f1ebb5c0f1c2ec5b2146edf0fce
Annie Truex <artruex@gmail.com>
2019-06-17 20:44:42 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,33 +1,42 @@
 class Triangle
-  # write code here	  # write code here
+  # write code here\
 
-attr_accessor :side1, :side2, :side3, :allsides
+  def initialize(s1, s2, s3)
 
-   def initialize(side1, side2, side3)
-    @allsides = []
-    @side1 = side1
-    @side2 = side2
-    @side3 = side3
+  
+    if s1 >= (s2 + s3) || s2 >= (s1 + s3) || s3 >= (s1 + s2)
+      raise TriangleError
+    end
+
+    if s1 <= 0 || s2 <= 0 ||s3 <= 0
+      raise TriangleError
+    end
+
+    if s1 == nil || s2 == nil ||s3 == nil
+      raise TriangleError
+    end
+
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
   end
 
-   def kind
-
-    if @side1 == @side2 && @side2 == @side3
+  def kind
+    if @s1 == @s2 && @s1 == @s3
       return :equilateral
-    elsif @side1 == @side2 && @side1 + @side2 > @side3 || @side1 == @side3 && @side1 + @side3 > @side2 || @side2 == @side3 && @side2 + @side3 > @side1
+    elsif @s1 == @s2 || @s2 == @s3 || @s3 == @s1
       return :isosceles
-    elsif @side1 != @side2 && @side2 != @side3 && @side1 + @side2 > @side3
-      return :scalene
     else
-      raise TriangleError 
-        #@side1 <=0 || @side2 <= 0 || @side3 <= 0
-      #end
-        end
-      end
-  
-   
-   class TriangleError < StandardError
-    def maessage
-      "not a triangle"
+      return :scalene
     end
+  end
+
+end
+
+
+
+class TriangleError < StandardError
+  def message
+    "This is not a valid triangle."
+  end
 end
